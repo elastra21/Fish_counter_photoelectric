ee78c57daeffa5643d5046169395702cee8dbfc6
Emmanuel Lastra <elastra@uabc.edu.mx>
2022-12-14 17:36:49 -0800

    Bigger Font for Workers

--- Screen.cpp
+++ Screen.cpp
@@ -55,21 +55,31 @@ void Screen::WorkersState(uint8_t no_workers) {
   const uint32_t background_color = TFT_YELLOW;
   const uint32_t text_color = TFT_BLACK;
 
-  M5.Lcd.setTextColor(text_color, background_color);
-  M5.Lcd.fillRoundRect(195, 5, 80, 30, 3, background_color);
+  // M5.Lcd.setTextColor(text_color, background_color);
+  // M5.Lcd.fillRoundRect(195, 5, 80, 30, 3, background_color);
+  // M5.Lcd.setTextSize(1);
+  // M5.Lcd.drawString("Workers", 215, 25);
+  // M5.Lcd.setTextSize(2);
+  // M5.Lcd.setTextDatum(TC_DATUM);
+  // M5.Lcd.drawString(String(no_workers), 235, 8);
+  // M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+  // M5.Lcd.setTextDatum(TL_DATUM);
+  
   M5.Lcd.setTextSize(1);
-  M5.Lcd.drawString("Workers", 215, 25);
-  M5.Lcd.setTextSize(2);
-  M5.Lcd.setTextDatum(TC_DATUM);
-  M5.Lcd.drawString(String(no_workers), 235, 8);
-  M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+  M5.Lcd.setTextDatum(TR_DATUM);
+  M5.Lcd.setFreeFont(&FreeSansBold12pt7b);
+  M5.Lcd.drawString("Workers", 300, 150);
+  M5.Lcd.setFreeFont(&FreeSansBold24pt7b);
+  // M5.Lcd.setCursor(X_STATUS, Y_COUNT + L_PADDING);
+  M5.Lcd.drawString(" "+String(no_workers), 300, Y_PROM + L_PADDING);
+  M5.Lcd.setFreeFont();
   M5.Lcd.setTextDatum(TL_DATUM);
 }
 
 void Screen::resetAvg() {
   M5.Lcd.setTextSize(1);
   M5.Lcd.setFreeFont(&FreeSansBold24pt7b);
-  M5.Lcd.drawString("0               ", X_STATUS, Y_PROM + L_PADDING);
+  M5.Lcd.drawString("0           ", X_STATUS, Y_PROM + L_PADDING);
   M5.Lcd.setFreeFont();
 }
 
@@ -77,7 +87,7 @@ void Screen::updateProm(float avg) {
   M5.Lcd.setTextSize(1);
   M5.Lcd.setFreeFont(&FreeSansBold24pt7b);
   // M5.Lcd.setCursor(X_STATUS, Y_PROM + L_PADDING);
-  M5.Lcd.drawString(String(avg < 1000 ? avg : 0)+"  ",X_STATUS, Y_PROM + L_PADDING);
+  M5.Lcd.drawString(String(avg < 1000 ? avg : 0) + "  ", X_STATUS, Y_PROM + L_PADDING);
   M5.Lcd.setFreeFont();
 }
 
@@ -85,6 +95,6 @@ void Screen::updateCount(const unsigned int value) {
   M5.Lcd.setTextSize(1);
   M5.Lcd.setFreeFont(&FreeSansBold24pt7b);
   // M5.Lcd.setCursor(X_STATUS, Y_COUNT + L_PADDING);
-  M5.Lcd.drawString(String(value)+"  ", X_STATUS, Y_COUNT + L_PADDING);
+  M5.Lcd.drawString(String(value) + "  ", X_STATUS, Y_COUNT + L_PADDING);
   M5.Lcd.setFreeFont();
 }
